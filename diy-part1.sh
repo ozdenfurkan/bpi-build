b9f21379f835398d8ba78f784f93a8a071e88a9c
Furkan Ozden <dev@furkanozden.com>
2026-05-23 19:29:29 +0200

    Fix: patch SD card size in filogic.mk

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-# Part 1: Runs before feeds update
-# Nothing needed for andrewjlamarche branch
+# SD card image boyutunu artır
+find target/linux/mediatek/image/ -name "filogic.mk" | xargs grep -l "sdcard\|SDCARD" | while read f; do
+    sed -i 's/bs=[0-9]*M conv=sync/bs=128M conv=sync/g' "$f"
+    sed -i 's/> [0-9]*[0-9])/> 999999999)/g' "$f"
+done
